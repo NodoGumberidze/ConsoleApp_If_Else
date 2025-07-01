ce207bc43bef11b38af2740e4953f9bb265154ae
Grigol <Grigol@DESKTOP-AN6NL9I>
2025-07-01 11:11:08 +0400

    Add project files.

---
+++ ConsoleApp_If_Else/Program.cs
@@ -0,0 +1,55 @@
+﻿// See https://aka.ms/new-console-template for more information
+// დავალება ნ1
+//int year;
+//Console.Write("Enter the year: ");
+//year = Convert.ToInt32(Console.ReadLine());
+
+//if(year % 4 ==0)
+//{
+//    Console.WriteLine("Es weli nakiania");
+//}
+//else
+//{
+//    Console.WriteLine("Es weli ar aris nakiani");
+//}
+
+// დავალება ნ2
+
+//int monthNumber;
+//int year;
+//Console.WriteLine("Sheikvanet tvis nomeri");
+//monthNumber = Convert.ToInt32(Console.ReadLine());
+//switch (monthNumber)
+//{
+//    case 1:
+//    case 3:
+//    case 5:
+//    case 7:
+//    case 8:
+//    case 10:
+//    case 12:
+//        Console.Write("This month has 31 days");
+//        break;
+//    case 2:
+//        Console.WriteLine("What year is it?");
+//        year = Convert.ToInt32(Console.ReadLine());
+//        if (year % 4 == 0)
+//        {
+//            Console.Write("This month has 29 days");
+//        }
+//        else
+//        {
+//            Console.Write("This month has 28 days");
+//        }
+//            break;
+//    case 4:
+//    case 6:
+//    case 9:
+//    case 11:
+//        Console.Write("This month has 30 days");
+//        break;
+//    default:
+//        Console.Write("Month with this number does not exist");
+//        break;
+//}
+
